6454afe70aa34e6ca6f158b24a7213734cea53c4
Gunvant Sharma <43336633+gunvant8008@users.noreply.github.com>
2022-11-09 15:10:38 +0000

    trending page done

---
+++ src/components/TrendingCoin.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+
+const TrendingCoin = ({ data }) => {
+  let navigate = useNavigate()
+
+  const getCoinDetails = id => {
+    navigate(`${id}`)
+  }
+
+  return (
+    <div onClick={() => getCoinDetails(data.item.id)} className=" w-[40%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40">
+      {data ? (
+        <>
+          <h3 className=" text-base flex items-center my-0.5 gap-2">
+            <span className=" capitalize text-gray-100">name: </span>
+            <span className="text-cyan">{data.item.name}</span>
+            <img src={data.item.small} alt={data.name} className=" w-[1.5rem] h-[1.5rem] mx-1.5 rounded-full" />
+          </h3>
+          <h3 className=" text-base flex items-center my-0.5 gap-2">
+            <span className=" capitalize text-gray-100">Market Cap Rank: </span>
+            <span className="text-cyan">{data.item.market_cap_rank}</span>
+          </h3>
+          <h3 className=" text-base flex items-center my-0.5 gap-2">
+            <span className=" capitalize text-gray-100">Price: </span>
+            <span className="text-cyan">
+              {new Intl.NumberFormat("en-IN", {
+                style: "currency",
+                currency: "btc",
+                maximumSignificantDigits: 5
+              }).format(data.item.price_btc)}
+            </span>
+          </h3>
+          <h3 className=" text-base flex items-center my-0.5 gap-2">
+            <span className=" capitalize text-gray-100">Score: </span>
+            <span className="text-cyan">{data.item.score}</span>
+          </h3>
+        </>
+      ) : null}
+    </div>
+  )
+}
+
+export default TrendingCoin
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,19 @@ import { Outlet } from "react-router-dom"
 import Logo from "../components/Logo"
 import Navigation from "../components/Navigation"
 import { CryptoProvider } from "../context/CryptoContext"
+import { TrendingProvider } from "../context/TrendingContext"
 
 const Home = () => {
   return (
     <CryptoProvider>
-      <main className="w-full h-full flex flex-col content-center items-center relative text-white font-nunito">
-        <div className=" w-screen h-screen bg-gray-300 fixed -z-10" />
-        <Navigation />
-        <Logo />
-        <Outlet />
-      </main>
+      <TrendingProvider>
+        <main className="w-full h-full flex flex-col content-center items-center relative text-white font-nunito">
+          <div className=" w-screen h-screen bg-gray-300 fixed -z-10" />
+          <Navigation />
+          <Logo />
+          <Outlet />
+        </main>
+      </TrendingProvider>
     </CryptoProvider>
   )
 }
